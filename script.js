f70507450a9578a513687ab0af2051a1952e5610
stephen etienne <sschneider725@gmail.com>
2024-09-23 15:12:02 -0400

    add validations

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ function showModal() {
     modal.classList.toggle('show')
     backdrop.classList.toggle('show')
 }
+
+const email = document.getElementById('email')
+const emailConfirm = document.getElementById('emailConfirm')
+const password = document.getElementById('password')
+const passwordConfirm = document.getElementById('passwordConfirm')
+const submit = document.getElementById('submit')
+
+submit.addEventListener('click', e => {
+    e.preventDefault();
+    emailValidation();
+    passwordValidation();
+})
+function emailValidation() {
+    if(email.value != emailConfirm.value) {
+        email.setCustomValidity('Email addresses do not match');
+        email.reportValidity();
+        emailConfirm.setCustomValidity('Email addresses do not match')
+    }else {
+        email.setCustomValidity('');
+        emailConfirm.setCustomValidity('')
+    }
+}
+function passwordValidation() {
+    if(password.value != passwordConfirm.value) {
+        password.setCustomValidity('passwords do not match');
+        password.reportValidity();
+        passwordConfirm.setCustomValidity('passwords do not match')
+    }else {
+        password.setCustomValidity('');
+        passwordConfirm.setCustomValidity('')
+    }
+}
